911b2f95515e67b11be59b017802f84049cdf48c
Ken Chen <kenchenpcpm@gmail.com>
2021-03-01 19:47:18 -0800

    quiz js for list questions and timer

---
+++ js/quiz.js
@@ -0,0 +1,100 @@
+var questions = [
+    {
+        title: " ",
+        choices: [" ", " ", " ", " "],
+        answer: " "
+    },
+    {
+        title: " ",
+        choices: [" ", " ", " ", " "],
+        answer: " "
+    },
+    {
+        title: " ",
+        choices: [" ", " ", " ", " "],
+        answer: " "
+    },
+    {
+        title: " ",
+        choices: [" ", " ", " ", " "],
+        answer: " "
+    },
+    {
+        title: " ",
+        choices: [" ", " ", " ", " "],
+        answer: " "
+    },
+    {
+        title: " ",
+        choices: [" ", " ", " ", " "],
+        answer: " "
+    },
+    {
+        title: " ",
+        choices: [" ", " ", " ", " "],
+        answer: " "
+    },
+    {
+        title: " ",
+        choices: [" ", " ", " ", " "],
+        answer: " "
+    },
+    {
+        title: " ",
+        choices: [" ", " ", " ", " "],
+        answer: " "
+    },
+    {
+        title: " ",
+        choices: [" ", " ", " ", " "],
+        answer: " "
+    },
+]
+var score = 0;
+var questionInd = 0;
+var starttime = document.querySelector("#starttime");
+var timer = document.querySelector("#startquiz");
+var questiondiv = document.querySelector("#questiondiv");
+var wrapper = document.querySelector("#wrapper");
+var secondleft = 100;
+var holdinterval = 0;
+var penalty = 10;
+var ulcreate = document.querySelector("ul");
+
+timer.addEventListener("click", function() {
+    if(holdinterval === 0){
+        holdinterval = setInterval(function(){
+            secondleft--;
+            starttime.textContent = "Time: " + secondleft;
+            if (secondleft <=0){
+                clearInterval(holdinterval);
+                done();
+                starttime.textContent = "DONE!";
+            }
+        }, 1000); 
+    }
+    list(questionInd)
+});
+
+function list(questionInd){
+    questiondiv.innerHTML = "";
+    ulcreate.innerHTML = "";
+    for (var i=0; i<questions.length; i++){
+        var studentquestion = questions[questionInd].title;
+        var studentchoices = questions[questionInd].choices;
+        questiondiv.textContent = studentquestion;
+    }
+    studentchoices.forEach(function(newlist){
+        var listitem = document.createElement("li");
+        listitem.textContent = newlist;
+        questiondiv.appendChild(ulcreate);
+        ulcreate.appendChild(listitem);
+        listitem.addEventListener("click", (compare));
+    })
+}
+function compare(event){
+
+}
+function done(){
+
+}
